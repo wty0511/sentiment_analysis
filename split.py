e68862a40f06f943c1d0a4c07447fa995c3fff81
wty0511 <15652198208@163.com>
2020-11-19 00:12:54 +0800

    no bert no data

--- split.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.utils import shuffle
-data=pd.read_csv('./data_balance.csv')
-data=shuffle(data)
-tran=data[:int(len(data)*0.7)]
-test=data[int(len(data)*0.7):len(data)]
-tran.to_csv('./train.csv',index=None,header=None)
-test.to_csv('./test.csv',index=None,header=None)
